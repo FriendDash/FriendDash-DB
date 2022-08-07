a3550d8782ccf255b778793dc67a378488243317
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-08-07 16:24:17 -0700

    change vercel

--- routes/stripe.js
+++ routes/stripe.js
@@ -12,7 +12,7 @@ router.post('/:stripeId/create-checkout-session', async (req, res) => {
     payment_method_types: ['card'],
     success_url:
       'https://frienddash-db.herokuapp.com/stripe/success?sessionId={CHECKOUT_SESSION_ID}',
-    cancel_url: 'https://frienddash.herokuapp.com/payment',
+    cancel_url: 'https://frienddash.vercel.app/payment',
     customer: req.params.stripeId,
   });
   res.redirect(session.url);
@@ -20,28 +20,27 @@ router.post('/:stripeId/create-checkout-session', async (req, res) => {
 
 router.get('/success', async (req, res) => {
   // const session = await stripe.checkout.sessions.retrieve(req.params.session_id);
-  res.redirect('https://frienddash.herokuapp.com/payment');
+  res.redirect('https://frienddash.vercel.app/payment');
 });
 
 router.get('/paymentMethods/:stripeId', async (req, res) => {
   if (req.params.stripeId === undefined) {
     return res.status(400).send('Bad id given');
   }
-  const paymentMethods = await stripe.customers.listPaymentMethods(
-    req.params.stripeId,
-    { type: 'card' }
-  ).then(response => res.send(response))
-  .catch(err => res.status(500));
+  const paymentMethods = await stripe.customers
+    .listPaymentMethods(req.params.stripeId, { type: 'card' })
+    .then(response => res.send(response))
+    .catch(err => res.status(500));
 });
 
 router.delete('/paymentMethods/:cardId', async (req, res) => {
   if (req.params.cardId === undefined || req.params.cardId === null) {
     return res.status(400).send('Bad card id given');
   }
-  const deletedCard = await stripe.paymentMethods.detach(
-    req.params.cardId
-  ).then(response => res.status(204).send(response))
-  .catch(err => res.status(500));
+  const deletedCard = await stripe.paymentMethods
+    .detach(req.params.cardId)
+    .then(response => res.status(204).send(response))
+    .catch(err => res.status(500));
 });
 
 router.post('/paymentMethods/:cardId', async (req, res) => {
@@ -51,12 +50,11 @@ router.post('/paymentMethods/:cardId', async (req, res) => {
   if (req.body == undefined || req.body == null) {
     return res.status(200);
   }
-  const updatedCard = await stripe.paymentMethods.update(
-    req.params.cardId,
-    req.body
-  ).then(response => res.send(response))
-  .catch(err => res.status(500));
-})
+  const updatedCard = await stripe.paymentMethods
+    .update(req.params.cardId, req.body)
+    .then(response => res.send(response))
+    .catch(err => res.status(500));
+});
 
 module.exports = router;
 
@@ -64,7 +62,7 @@ router.post('/accountLink/:id', async (req, res) => {
   if (req.params.id == undefined || req.params.id == null) {
     return res.status(400);
   }
-  
+
   const accountLink = await stripe.accountLinks.create({
     account: req.params.id,
     refresh_url: 'http://localhost:3000/payment',
@@ -79,28 +77,35 @@ router.get('/accounts/:id', async (req, res) => {
     return res.status(400);
   }
 
-  const account = await stripe.accounts.retrieve(
-    req.params.id
-  ).then(response => res.send(response))
-  .catch(err => res.status(500));
+  const account = await stripe.accounts
+    .retrieve(req.params.id)
+    .then(response => res.send(response))
+    .catch(err => res.status(500));
 });
 
 router.post('/checkout', async (req, res) => {
-  if (!req.body.userStripeId || !req.body.amount || !req.body.paymentMethodId || !req.body.receiverId) {
+  if (
+    !req.body.userStripeId ||
+    !req.body.amount ||
+    !req.body.paymentMethodId ||
+    !req.body.receiverId
+  ) {
     return res.status(400).send({ message: 'missing body fields' });
   }
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: 'cad',
-    confirm: true,
-    customer: req.body.userStripeId,
-    payment_method: req.body.paymentMethodId,
-    transfer_data: {
+  const paymentIntent = await stripe.paymentIntents
+    .create({
       amount: req.body.amount,
-      destination: req.body.receiverId
-    },
-    return_url: 'https://frienddash.herokuapp.com',
-  }).then (response => res.send(response))
-  .catch(err => res.send(500));
+      currency: 'cad',
+      confirm: true,
+      customer: req.body.userStripeId,
+      payment_method: req.body.paymentMethodId,
+      transfer_data: {
+        amount: req.body.amount,
+        destination: req.body.receiverId,
+      },
+      return_url: 'https://frienddash.vercel.app/',
+    })
+    .then(response => res.send(response))
+    .catch(err => res.send(500));
 });
